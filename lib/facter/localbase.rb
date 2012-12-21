8283a86fa936e4f2e4eda20f9b40a4c5e583851d
Garrett Wollman <wollman@csail.mit.edu>
2012-12-20 21:45:20 -0500

    freebsd: add a facter plugin to identify where LOCALBASE is
    This assumes that Puppet is installed in the same place as everything
    else.  Having this means that we can depend on $::localbase always
    being defined on FreeBSD, so we don't have to hard-code "/usr/local"
    in paths everywhere.  Should be a standard part of the FreeBSD puppet
    port, which will make the resolution trivial (we can just patch the
    answer in when the port is built) but may take a bit of convincing.

---
+++ lib/facter/localbase.rb
@@ -0,0 +1,9 @@
+# Figure out where packages are installed (normally /usr/local but this
+# is configurable).  Assume that the puppet package is installed in the
+# same place as other packages.
+Facter.add(:localbase ) do
+  confine :osfamily => [ :FreeBSD ]  
+  setcode do
+    Facter::Util::Resolution.exec('pkg query "%p" puppet')
+  end
+end
