462cc4cee78f1379c33602773da54a27dae475a4
Garrett Wollman <wollman@csail.mit.edu>
2015-08-21 15:21:41 -0400

    freebsd::periodic_conf: minor robustness and convenience improvements
    Add some documentation, and functionally separate the SETTING to be
    updated from the resource title, in accordance with standard Puppet
    behavior.  When removing a setting, allow the VALUE to be left
    undefined, since it doesn't make any sense to define a value for
    something that doesn't exist, but when adding a setting, make sure the
    VALUE is actually a string and not some random other data type.

--- manifests/periodic_conf.pp
+++ manifests/periodic_conf.pp
@@ -1,8 +1,20 @@
-# From http://projects.puppetlabs.com/projects/1/wiki/Puppet_Free_Bsd
-define freebsd::periodic_conf($ensure, $value) {
+#
+# freebsd::periodic_conf defines things in /etc/periodic.conf, which
+# controls the operation of periodic(8) (run hourly, daily, weekly, and
+# monthly for different tasks from cron).  If ENSURE is set to
+# present (the default), then the periodic.conf setting SETTING will be
+# defined to VALUE.  If ENSURE is set to 'absent' or 'purged', then
+# SETTING will be unset (restoring the default behavior).  SETTING defaults
+# to the name of the resource.
+# 
+define freebsd::periodic_conf($setting = $name, $ensure = 'present',
+			      $value = undef) {
+  if $ensure == 'present' {
+    validate_string($value)
+  }
   shell_config {  "periodic_conf_${name}":
     file => '/etc/periodic.conf',
-    key => $name,
+    key => $setting,
     value => $value,
     ensure => $ensure
   }
