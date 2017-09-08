ced8e60799f4d77261b0d3f03c7b67bd5e1bf784
Garrett Wollman <wollman@csail.mit.edu>
2017-09-08 11:02:04 -0400

    localbase: reference pkg package, not puppet38 package
    The localbase fact is determined by checking the PREFIX of some
    installed package.  We were looking for puppet38, which is clearly
    wrong if you're running puppet4 (or later).  Ideally, this fact would
    just be defined by the facter package itself, and would capture the
    value of LOCALBASE used for port builds, but looking for where pkg
    itself is installed seems like the next-best option.  (We can't look
    for the facter package itself because there is also more than one of
    those.)

--- lib/facter/localbase.rb
+++ lib/facter/localbase.rb
@@ -1,9 +1,9 @@
 # Figure out where packages are installed (normally /usr/local but this
-# is configurable).  Assume that the puppet package is installed in the
+# is configurable).  Assume that the pkg package is installed in the
 # same place as other packages.
 Facter.add(:localbase ) do
   confine :osfamily => [ :FreeBSD ]  
   setcode do
-    Facter::Util::Resolution.exec('pkg query "%p" puppet38')
+    Facter::Util::Resolution.exec('pkg query "%p" pkg')
   end
 end
